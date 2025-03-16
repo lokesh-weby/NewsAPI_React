6e6e72fdc18a7f6a23c87812f27ac6354d4e3a17
[lokesh-weby] <lokesh0212004@gmail.com>
2025-03-16 12:56:15 +0530

    Search Engine Opitimism

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,18 @@ const App = () => {
   useEffect(()=>{
   const api="4ba6252c757c4f928b7362a0ec4511b4";
 
-  async function GetNews() {
+  try{
+    async function GetNews() {
       const response=await fetch(`https://newsapi.org/v2/top-headlines?category=${selectedCategory}&apiKey=${api}`);
       const data=await response.json();
       setNews(data.articles); 
   }
-      GetNews();      
+      GetNews();   
+  }
+  catch(e){
+    console.log(e);
+  }
+    
   },[selectedCategory])
 
   console.log(news);
@@ -38,7 +44,7 @@ const App = () => {
       {news===""?<h1>loading</h1>:(
          news.map((newsData,index)=>(
           <div key={index} className='card'>
-            <img src={newsData.urlToImage} alt={newsData.author} height={200} width={200} loading='lazy'/>
+            <img src={newsData.urlToImage} alt={newsData.author}  loading='lazy'/>
             <p className='title'>{(index+1)+". "+newsData.title}</p>
             <p>{(newsData.description)|| newsData.title}..<a href={newsData.url} target='_blank'> ReadMore</a></p>
             <small>{"By "+newsData.author}</small>
